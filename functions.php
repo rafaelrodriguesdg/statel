c245351900bb0bacaf4773d0e17a2e6a6b91a508
Rafael Rodrigues <rafaelrodrigues.dg@gmail.com>
2017-09-07 16:11:15 -0300

    update no functions

--- functions.php
+++ functions.php
@@ -4,3 +4,12 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+function my_et_builder_post_types( $post_types ) {
+    $post_types[] = 'cuar_private_file';
+    $post_types[] = 'cuar_private_page';
+    $post_types[] = 'private-page';
+     
+    return $post_types;
+}
+add_filter( 'et_builder_post_types', 'my_et_builder_post_types' );
