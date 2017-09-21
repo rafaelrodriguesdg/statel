a7908327008b7b55416d7072321b8e0383dd8fa3
Rafael Rodrigues <rafaelrodrigues.dg@gmail.com>
2017-09-21 20:51:04 -0300

    adicionada funcao shortcode

--- functions.php
+++ functions.php
@@ -13,3 +13,18 @@ function my_et_builder_post_types( $post_types ) {
     return $post_types;
 }
 add_filter( 'et_builder_post_types', 'my_et_builder_post_types' );
+
+// Shortcode para exibir o usuário logado
+
+function show_loggedin_function( $atts ) {
+    
+    global $current_user, $user_login;
+          get_currentuserinfo();
+    add_filter('widget_text', 'do_shortcode');
+    if ($user_login) 
+    return 'Welcome ' . $current_user->display_name . '!';
+    else
+    return '<a href="' . wp_login_url() . ' ">Login</a>';
+    
+    }
+    add_shortcode( 'show_loggedin_as', 'show_loggedin_function' );
