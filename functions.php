d33907d624c822b7cab65c65b2892da450f31c23
Rafael Rodrigues <rafaelrodrigues.dg@gmail.com>
2017-09-22 00:21:38 -0300

    Adicionadas funções php e personalização css da pagina de membros

--- functions.php
+++ functions.php
@@ -14,17 +14,22 @@ function my_et_builder_post_types( $post_types ) {
 }
 add_filter( 'et_builder_post_types', 'my_et_builder_post_types' );
 
-// Shortcode para exibir o usuário logado
+// Shortcode para exibir informações do usuário logado
 
 function show_loggedin_function( $atts ) {
     
     global $current_user, $user_login;
-          get_currentuserinfo();
+    wp_get_current_user();
+    
+    $user_info = get_userdata($current_user->ID);
+    $user_role = implode(', ', $user_info->roles);
+
+
     add_filter('widget_text', 'do_shortcode');
     if ($user_login) 
-    return 'Welcome ' . $current_user->display_name . '!';
+        return '<h4>' . $current_user->display_name . '</h4><p>' . $current_user->user_email . ' <span style="text-transform:uppercase; font-style:oblique;">(' . $user_role . ')</span></p>';
     else
-    return '<a href="' . wp_login_url() . ' ">Login</a>';
+        return '<a href="' . wp_login_url() . ' ">Login</a>';
     
     }
-    add_shortcode( 'show_loggedin_as', 'show_loggedin_function' );
+add_shortcode( 'show_loggedin_as', 'show_loggedin_function' );
