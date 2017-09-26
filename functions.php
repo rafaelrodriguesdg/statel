9547fba2e1c9cb97f11a93e229df179bb0837ed3
Rafael Rodrigues <rafaelrodrigues.dg@gmail.com>
2017-09-26 00:29:48 -0300

    função mostrar avatar do usuário

--- functions.php
+++ functions.php
@@ -31,3 +31,18 @@ function show_loggedin_function( $atts ) {
     
     }
 add_shortcode( 'show_loggedin_as', 'show_loggedin_function' );
+
+// Shortcode para exibir avatar do usuário logado
+
+function shortcode_user_avatar() {
+    if(is_user_logged_in()) { // check if user is logged in
+        global $current_user; // get current user's information
+        get_currentuserinfo();
+        return get_avatar( $current_user -> ID, 24 ); // display the logged-in user's avatar
+    }
+    else {
+      // if not logged in, show default avatar. change URL to show your own default avatar
+        return get_avatar( 'http://1.gravatar.com/avatar/ad524503a11cd5ca435acc9bb6523536?s=64', 24 );
+    }
+}
+add_shortcode('display-user-avatar','shortcode_user_avatar');
